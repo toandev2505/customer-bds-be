5e47a230ae25a4d5571120ec0405da714f320d74
Toan Truong <toandev2505@gmail.com>
2026-03-08 21:53:57 +0700

    init project

---
+++ src/main/java/com/chude2/chude2/Chude2Application.java
@@ -0,0 +1,13 @@
+package com.chude2.chude2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chude2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chude2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chude2/chude2/Chude2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chude2.chude2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chude2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
