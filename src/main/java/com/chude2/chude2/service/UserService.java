e1952900ad6ea350a0d22285c86b896ba365f8f5
Toan Dev <toandev2505@gmail.com>
2026-03-13 15:31:53 +0700

    add more CRUD api user

--- src/main/java/com/chude2/chude2/service/UserService.java
+++ src/main/java/com/chude2/chude2/service/UserService.java
@@ -4,15 +4,30 @@ import com.chude2.chude2.entity.UserEntity;
 import com.chude2.chude2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class UserService {
-@Autowired
+    @Autowired
     private UserRepository userRepository;
-public List<UserEntity> findAll(){
-    List<UserEntity> entities=userRepository.findAll();
-    return entities;
-}
+    public List<UserEntity> findAll(){
+        return userRepository.findAll();
+    }
+    
+    public UserEntity findOneById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id " + id));
+    }
+
+    @Transactional
+    public UserEntity save(UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    @Transactional
+    public void deleteList(List<Long> ids){
+        userRepository.deleteAllById(ids);
+    }
 }
