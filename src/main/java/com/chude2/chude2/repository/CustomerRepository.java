861b49facc6158abb4d8e7dde402ddc8536e3507
thanhtan <tathanhtan2608@gmail.com>
2026-04-07 17:06:24 +0700

    Xoamem_Khoiphuc

--- src/main/java/com/chude2/chude2/repository/CustomerRepository.java
+++ src/main/java/com/chude2/chude2/repository/CustomerRepository.java
@@ -8,5 +8,11 @@ import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+    @Modifying
+    @Query("UPDATE CustomerEntity c SET c.status = 0 WHERE c.id IN :ids")
+    void softDeleteByIds(@Param("ids") List<Long> ids);
+    @Modifying
+    @Query("UPDATE CustomerEntity c SET c.status = 1 WHERE c.id IN :ids")
+    void recoverByIds(@Param("ids") List<Long> ids);
     List<CustomerEntity> findAllByStatus(int status);
 }
