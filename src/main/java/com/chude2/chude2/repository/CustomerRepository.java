cc8f5cab9d68e5d9ea0714cb9ca44b4edb4ec726
Toan Truong <toandev2505@gmail.com>
2026-04-08 19:28:20 +0700

    Add import statements for query and modifying annotations

--- src/main/java/com/chude2/chude2/repository/CustomerRepository.java
+++ src/main/java/com/chude2/chude2/repository/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.chude2.chude2.repository;
 import com.chude2.chude2.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
